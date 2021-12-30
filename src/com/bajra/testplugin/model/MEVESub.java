27eddfa02c5062a77b4bf7d4921f5045ca36be55
mashudwahyu <mashudwahyu04@gmail.com>
2021-12-30 14:23:37 +0700

    update

--- src/com/bajra/testplugin/model/MEVESub.java
+++ src/com/bajra/testplugin/model/MEVESub.java
@@ -3,6 +3,7 @@ package com.bajra.testplugin.model;
 import java.sql.ResultSet;
 import java.util.Properties;
 
+
 public class MEVESub extends X_EVE_Sub{
 
 	/**
@@ -18,5 +19,12 @@ public class MEVESub extends X_EVE_Sub{
 	public MEVESub(Properties ctx, ResultSet rs, String trxName) {
 		super(ctx, rs, trxName);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public MEVESub(MEVEMain main) {
+		super(main.getCtx(), 0, main.get_TrxName());
+		setAD_Org_ID(main.getAD_Org_ID());
+		
+		setEVE_Main_ID(main.getEVE_Main_ID());
 	}
 }
